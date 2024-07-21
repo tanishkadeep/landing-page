1a4c5724821bee63ce54fbde679eeb384178c2ae
Tanishka <tanishkadeep09@gmail.com>
2024-07-22 04:14:03 +0530

    header ui update

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center px-6 py-4 backdrop-blur-md sticky top-0 z-20">
+    <header className="flex justify-between items-center px-6 py-4 backdrop-blur-md sticky top-0 z-20 bg-gradient-to-r from-[#E0E7FD] to-[#FDFEFF] shadow-md">
       <Image src={Logo} alt="Logo" />
       <FaBars className="block md:hidden" />
       <nav className="hidden md:block">
