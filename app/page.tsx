7c6080c0cc69af399c5c8f7fe5d570951b713cb3
Tanishka <tanishkadeep09@gmail.com>
2024-07-22 02:33:52 +0530

    product showcase ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import BrandSlide from "@/components/BrandSlide";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import ProductShowcase from "@/components/ProductShowcase";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero/>
       <BrandSlide/>
+      <ProductShowcase/>
     </div>
   );
 }
---
+++ components/ProductShowcase.tsx
@@ -0,0 +1,90 @@
+import ProductImage from "@/public/assests/Product Image.png";
+import Pyramid from "@/public/assests/pyramid.png";
+import Tube from "@/public/assests/tube.png";
+import Image from "next/image";
+import { FaArrowRight } from "react-icons/fa";
+import { GoBell, GoGoal } from "react-icons/go";
+import { LuLeaf } from "react-icons/lu";
+import { MdLockOutline } from "react-icons/md";
+
+const ProductShowcase = () => {
+  return (
+    <div className="bg-gradient-to-t from-[#acbae8] to-white flex flex-col items-center overflow-x-hidden pb-24">
+      <div className="flex flex-col items-center font-medium mt-24 px-8 mx-auto md:w-[550px] lg:w-[630px]">
+        <div className="border-2 w-fit p-0.5 px-3 text-sm rounded-xl border-slate-300/80">
+          Boost your productivity
+        </div>
+        <div className="text-3xl md:text-4xl lg:text-5xl mt-6 mb-5 font-bold tracking-tighter text-center">
+          A more effective way to track progress
+        </div>
+
+        <div className="text-center text-lg mb-8 md:text-xl">
+          Effortlessly turn your ideas into a fully functional, responsive,
+          no-code SaaS website in just minutes with the set of free components
+          for Framer.
+        </div>
+      </div>
+      <div className="relative ">
+        <Image
+          src={Pyramid}
+          alt="Pyramid Image"
+          className="absolute -right-24 -top-20 w-72 h-72 hidden md:block"
+        />
+        <Image src={ProductImage} alt="Product Image" className="px-1" />
+        <Image
+          src={Tube}
+          alt="Tube Image"
+          className="absolute w-72 h-72 top-64 -left-28 hidden md:block"
+        />
+      </div>
+
+      <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-20 max-w-[1400px] lg:px-28">
+        <div className="mt-16">
+          <LuLeaf className="text-2xl mb-3" />
+          <div className="font-bold text-2xl">Integration ecosystem</div>
+          <div className="text-lg my-2">
+            Track your progress and motivate your efforts everyday.
+          </div>
+          <div className="text-lg font-medium">
+            Learn more <FaArrowRight className="inline h-3 w-3" />
+          </div>
+        </div>
+
+        <div className="mt-16">
+          <GoGoal className="text-2xl mb-3" />
+          <div className="font-bold text-2xl">Goal setting and tracking</div>
+          <div className="text-lg my-2">
+            Set and track goals with manageable task breakdowns.
+          </div>
+          <div className="text-lg font-medium">
+            Learn more <FaArrowRight className="inline h-3 w-3" />
+          </div>
+        </div>
+
+        <div className="mt-16">
+          <MdLockOutline className="text-2xl mb-3" />
+          <div className="font-bold text-2xl">Secure data encryption</div>
+          <div className="text-lg my-2">
+            Ensure your data's safety with top-tier encryption.
+          </div>
+          <div className="text-lg font-medium">
+            Learn more <FaArrowRight className="inline h-3 w-3" />
+          </div>
+        </div>
+
+        <div className="mt-16">
+          <GoBell className="text-2xl mb-3" />
+          <div className="font-bold text-2xl">Customizable notifications</div>
+          <div className="text-lg my-2">
+            Get alerts on tasks and deadlines that matter most.
+          </div>
+          <div className="text-lg font-medium">
+            Learn more <FaArrowRight className="inline h-3 w-3" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductShowcase;
